eb8b4a5c8db91a5408c7331f345516ebdc34e54b
Andy Ballinger <a.ballinger@codat.io>
2024-01-24 13:53:52 +0000

    restored msa page

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="docs/faqs">
+            to="./faqs">
             Codat Legal Docs 📜
           </Link>
         </div>
